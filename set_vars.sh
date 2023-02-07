fa98befb719c34b72f4d267d6bfbd07ee8773dd2
NLenzini <120989595+NLenzini@users.noreply.github.com>
2023-02-07 02:20:45 +0000

    Tweaks set_vars.sh

--- set_vars.sh
+++ set_vars.sh
@@ -1,4 +1,4 @@
 readarray -t lines < spotify.cfg
-export SPOTIFY_CLIENT_ID=${lines[0]}
-export SPOTIFY_CLIENT_SECRET=${lines[1]}
-export SPOTIFY_REDIRECT_URI=${lines[2]}
+export SPOTIPY_CLIENT_ID=${lines[0]}
+export SPOTIPY_CLIENT_SECRET=${lines[1]}
+export SPOTIPY_REDIRECT_URI=${lines[2]}
