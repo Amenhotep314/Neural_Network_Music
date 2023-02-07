b587df0db2771754e4790b3349a7ec22da93e986
NLenzini <120989595+NLenzini@users.noreply.github.com>
2023-02-07 02:31:43 +0000

    Updates authentication process

--- set_vars.sh
+++ set_vars.sh
@@ -1,4 +1,1 @@
-readarray -t lines < spotify.cfg
-export SPOTIPY_CLIENT_ID=${lines[0]}
-export SPOTIPY_CLIENT_SECRET=${lines[1]}
-export SPOTIPY_REDIRECT_URI=${lines[2]}
+readarray -t lines < spotify.cfg && export SPOTIPY_CLIENT_ID=${lines[0]} && export SPOTIPY_CLIENT_SECRET=${lines[1]} && export SPOTIPY_REDIRECT_URI=${lines[2]}
