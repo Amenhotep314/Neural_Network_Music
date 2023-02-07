386a02bb1a9f938de3ba5bd3a3750ef18e6c7c4d
NLenzini <120989595+NLenzini@users.noreply.github.com>
2023-02-07 03:23:09 +0000

    Sets up interactive sample collection

--- Data_Collection.py
+++ Data_Collection.py
@@ -1,5 +1,6 @@
 import spotipy
 import json
+import requests
 import os
 import subprocess
 
@@ -10,16 +11,33 @@ import miditok
 with open("spotify.cfg") as credentials:
 
     content = credentials.readlines()
-    SPOTIPY_CLIENT_ID = content[0]
-    SPOTIPY_CLIENT_SECRET = content[1]
-    SPOTIPY_REDIRECT_URI = content[2]
+    # SPOTIPY_CLIENT_ID = content[0]
+    # SPOTIPY_CLIENT_SECRET = content[1]
+    # SPOTIPY_REDIRECT_URI = content[2]
     username = content[3]
 
 
 def main():
 
     token = spotipy.util.prompt_for_user_token(username)
+    spotify = spotipy.Spotify(auth=token)
+
+    track = spotify.track(input("URI: "))
+    print(json.dumps(track, sort_keys=True, indent=4))
+
+    try:
+        url = track["preview_url"]
+    except:
+        print("No preview found.")
+        url = ""
+
+    if url:
+        web_file = requests.get(url, allow_redirects=True)
+        name = str(track["popularity"]) + "_" + track["name"].replace(" ", "_") + ".mp3"
+        with open(os.path.join("Tracks", name), 'wb') as local_file:
+            local_file.write(web_file.content)
 
 if __name__ == "__main__":
 
-    main()
+    while True:
+        main()
