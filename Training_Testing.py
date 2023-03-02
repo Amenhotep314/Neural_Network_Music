58f037f95529130616a2ba67fd2f32a0ca57683c
NLenzini <nathan.lenzini@stjlabs.com>
2023-03-02 14:31:12 -0500

    Expands neural network assessment tools

--- Training_Testing.py
+++ Training_Testing.py
@@ -82,18 +82,103 @@ def train_model__03_02_2023():
     model.save("Model_03-02-2023")
 
 
-def load_data_sets():
+def test_model(model_name="Model", test_only=False):
+
+    """Assesses the accuracy of a fitted model.
+    Args:
+        model_name (str): The folder name under which the model was saved. (Default is Model)
+        test_only (bool): Should the function try only the test data set, or each in turn? (Default is False)"""
+
+    # Load the data and the trained model from the filesystem
+    model = tensorflow.keras.models.load_model(model_name)
+    shape = model.layers[0].input_shape[1]
+    train_data, train_labels, test_data, test_labels = load_data_sets(shape=shape)
+
+    print("Assessing model accuracy.")
+
+    # Try it on the training data too, to look for overfitting
+    if not test_only:
+        results = model.evaluate(train_data, train_labels)
+        print("Train data performance:\nLoss: " + str(results[0]) + "\tAccuracy: " + str(results[1]))
+
+    # And of course check performance on the test set
+    results = model.evaluate(test_data, test_labels)
+    print("Test data performance:\nLoss: " + str(results[0]) + "\tAccuracy: " + str(results[1]))
+
+
+def test_model_individually(model_name="Model", test_only=False):
+
+    """Applies the model individually and verbosely to each element of the data set.
+    Args:
+        model_name (str): The folder name under which the model was saved. (Default is Model)
+        test_only (bool): Should the function try only the test data set, or each in turn? (Default is False)"""
+
+    # Load the data and the trained model from the filesystem
+    model = tensorflow.keras.models.load_model(model_name)
+    shape = model.layers[0].input_shape[1]
+    train_data, train_labels, test_data, test_labels = load_data_sets(shape=shape)
+
+    print("Assessing model accuracy individually.")
+
+    pred_accuracies = []
+    rand_accuracies = []
+
+    # Try it on the training data too, to look for overfitting
+    if not test_only:
+        print("\t".join(["Actual", "Predicted", "Random", "Predicted Error", "Random Error"]))
+        for i in range(len(train_data)):
+            actual = train_labels[i]
+            predicted = numpy.argmax(model.predict(train_data[i][numpy.newaxis], verbose=0)[0])
+            randomized = random.randint(0, 99)
+            pred_accuracy = abs(predicted - actual)
+            rand_accuracy = abs(randomized - actual)
+
+            pred_accuracies.append(pred_accuracy)
+            rand_accuracies.append(rand_accuracy)
+            print("\t".join([str(actual), str(predicted), str(randomized), str(pred_accuracy), str(rand_accuracy)]))
+
+        # This is pretty simple: add up the percent errors and average them to get an idea of how often it's close
+        average_pred = sum(pred_accuracies) / len(pred_accuracies)
+        average_rand = sum(rand_accuracies) / len(rand_accuracies)
+        print("Train data performance:\nAverage predicted error: " + str(average_pred) + "\tAverage randomized error: " + str(average_rand))
+        pred_accuracies = []
+        rand_accuracies = []
+
+    # Do this to the test data
+    print("\t".join(["Actual", "Predicted", "Random", "Predicted Error", "Random Error"]))
+    for i in range(len(test_data)):
+        actual = test_labels[i]
+        predicted = numpy.argmax(model.predict(test_data[i][numpy.newaxis], verbose=0)[0])
+        randomized = random.randint(0, 99)
+        pred_accuracy = abs(predicted - actual)
+        rand_accuracy = abs(randomized - actual)
+
+        pred_accuracies.append(pred_accuracy)
+        rand_accuracies.append(rand_accuracy)
+        print("\t".join([str(actual), str(predicted), str(randomized), str(pred_accuracy), str(rand_accuracy)]))
+
+    average_pred = sum(pred_accuracies) / len(pred_accuracies)
+    average_rand = sum(rand_accuracies) / len(rand_accuracies)
+    print("Test data performance:\nAverage predicted error: " + str(average_pred) + "\tAverage randomized error: " + str(average_rand))
+
+
+def load_data_sets(shape=None):
 
     """Loads training and test data into useful formats.
+    Args:
+        shape (int): The length of each numpy array. If None, the shape will be drawn from the data set. Default is None
     Returns:
         train data, train labels, test data, test labels"""
 
     train_data, train_labels = load_data_set("train")
     test_data, test_labels = load_data_set("test")
 
-    train_longest = max(train_data, key=lambda x: len(x))
-    test_longest = max(test_data, key=lambda x: len(x))
-    length = len(train_longest) if len(train_longest) > len(test_longest) else len(test_longest)
+    if not shape:
+        train_longest = max(train_data, key=lambda x: len(x))
+        test_longest = max(test_data, key=lambda x: len(x))
+        length = len(train_longest) if len(train_longest) > len(test_longest) else len(test_longest)
+    else:
+        length = shape
 
     for i in range(len(train_data)):
         item = numpy.array(train_data[i])
@@ -105,7 +190,14 @@ def load_data_sets():
         item = numpy.resize(item, length)
         test_data[i] = item
 
-    return numpy.array(train_data), numpy.array(train_labels), numpy.array(test_data), numpy.array(test_labels)
+    train_data = numpy.array(train_data)
+    train_data = numpy.reshape(train_data, (train_data.shape[0], train_data.shape[1], 1))
+    train_labels = numpy.array(train_labels)
+    test_data = numpy.array(test_data)
+    test_data = numpy.reshape(test_data, (test_data.shape[0], test_data.shape[1], 1))
+    test_labels = numpy.array(test_labels)
+
+    return train_data, train_labels, test_data, test_labels
 
 
 def load_data_set(data_set):
@@ -130,27 +222,6 @@ def load_data_set(data_set):
             labels.append(label)
 
     return datas, labels
-
-
-def test_model(model_name="Model"):
-
-    """Assesses the accuracy of a fitted model.
-    Args:
-        model_name (str): The folder name under which the model was saved. (Default is Model)"""
-
-    # Load the data and the trained model from the filesystem
-    train_data, train_labels, test_data, test_labels = load_data_sets()
-    model = tensorflow.keras.models.load_model(model_name)
-
-    print("Assessing model accuracy.")
-
-    # Try it on the training data too, to look for overfitting
-    results = model.evaluate(train_data, train_labels)
-    print("Train data performance:" + "\n" + "Loss: " + str(results[0]) + "\t" "Accuracy: " + str(results[1]))
-
-    # And of course check performance on the test set
-    results = model.evaluate(test_data, test_labels)
-    print("Test data performance:" + "\n" + "Loss: " + str(results[0]) + "\t" "Accuracy: " + str(results[1]))
 
 
 if __name__ == "__main__":
